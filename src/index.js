12fead082e087abf4d77457a538b6d9a6d92a347
Diego Rubio <darubiomunoz@gmail.com>
2021-03-25 20:33:09 -0300

    Fetching data from bin

--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+const DATA_URL = "https://api.jsonbin.io/b/605d1c7c7c9f775f63899095";
+
+const fetchData = async URL => {
+  const response = await fetch(URL);
+  if(!response.ok) console.log(`An error has occurred: ${response.status}`);
+  const data = await response.json();
+  return data;
+}
+
+fetchData(DATA_URL);
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 reportWebVitals();
