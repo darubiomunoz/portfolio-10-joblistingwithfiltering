85814f21da08676143dbbfdc46dccddf7968b76a
Diego Rubio <darubiomunoz@gmail.com>
2021-03-28 16:58:27 -0300

    Logo image for the job offer component

--- src/components/JobOffer.js
+++ src/components/JobOffer.js
@@ -4,7 +4,9 @@ import './styles/JobOffer.css';
 const JobOffer = () => {
   return (
     <div className="joboffer">
-
+      <figure className="joboffer__image--container">
+        <img className="joboffer__logo" src="https://i.postimg.cc/Y2xsG7wk/photosnap.png" loading="lazy" alt="company's logo" />
+      </figure>
     </div>
   );
 }
