262590ff853d9f31e7411221305bc6f15ff22e78
Diego Rubio <darubiomunoz@gmail.com>
2021-03-28 17:49:36 -0300

    html structure for job offers

--- src/components/JobOffer.js
+++ src/components/JobOffer.js
@@ -5,8 +5,31 @@ const JobOffer = () => {
   return (
     <div className="joboffer">
       <figure className="joboffer__image--container">
-        <img className="joboffer__logo" src="https://i.postimg.cc/Y2xsG7wk/photosnap.png" loading="lazy" alt="company's logo" />
+        <img
+          className="joboffer__logo"
+          src="https://i.postimg.cc/Y2xsG7wk/photosnap.png"
+          loading="lazy"
+          alt="company's logo"
+        />
       </figure>
+      <div className="joboffer__item">
+        <section className="joboffer__info">
+          <h2 className="joboffer__companyname">Company Name</h2>
+          <h3 className="joboffer__title">Job title</h3>
+          <div className="joboffer__details">
+            <p className="joboffer__status">1d ago</p>
+            <p className="joboffer__status">Fulltime</p>
+            <p className="joboffer__status">USA only</p>
+          </div>
+        </section>
+        <section className="joboffer__keywords">
+          <p className="joboffer__badge">Frontend</p>
+          <p className="joboffer__badge">Senior</p>
+          <p className="joboffer__badge">HTML</p>
+          <p className="joboffer__badge">CSS</p>
+          <p className="joboffer__badge">JavaScript</p>
+        </section>
+      </div>
     </div>
   );
 }
