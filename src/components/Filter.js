f1b89e2c6ba3496bd8aca75c217f3b57eb161728
Diego Rubio <darubiomunoz@gmail.com>
2021-03-31 13:07:36 -0300

    fixing bug in the styles of the job offers

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { nanoid } from '@reduxjs/toolkit';
+import { nanoid } from "@reduxjs/toolkit";
 import "./styles/Filter.css";
 
 const Filter = () => {
@@ -8,19 +8,23 @@ const Filter = () => {
   console.log(filters);
 
   return (
-    <div className="filter">
-      <div className="filter__categories">
-        {filters.map((filter) => {
-          return (
-            <div className="filter__badge" key={nanoid()}>
-              <p className="filter__name">{filter}</p>
-              <button className="filter__button--badge">X</button>
-            </div>
-          );
-        })}
-      </div>
-      <button className="filter__button">Clear</button>
-    </div>
+    <>
+      {filters.length > 0 && (
+        <div className="filter">
+          <div className="filter__categories">
+            {filters.map((filter) => {
+              return (
+                <div className="filter__badge" key={nanoid()}>
+                  <p className="filter__name">{filter}</p>
+                  <button className="filter__button--badge">X</button>
+                </div>
+              );
+            })}
+          </div>
+          <button className="filter__button">Clear</button>
+        </div>
+      )}
+    </>
   );
 };
 
